11b9581bb5774b76e3b92b7f050f8d746be3a5ce
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-01-15 08:17:15 +0000

    up themes

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -2,4 +2,6 @@ import { StreamLanguage } from '@codemirror/language';
 // import { StreamLanguage } from '@codemirror/legacy-modes';
 // import go from '@codemirror/legacy-modes/mode/go';
 // import {lua} from "@codemirror/legacy-modes/mode/lua";
-import { to
+import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
+import CodeMirror from '@uiw/react-codemirror';
+import { FC, useEffec
