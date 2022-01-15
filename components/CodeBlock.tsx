df3f6d9fe5631a8e5b3b91bcb41076f227350592
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-01-15 07:09:37 +0000

    update portfolio

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,3 +1,5 @@
 import { StreamLanguage } from '@codemirror/language';
 // import { StreamLanguage } from '@codemirror/legacy-modes';
-//
+// import go from '@codemirror/legacy-modes/mode/go';
+// import {lua} from "@codemirror/legacy-modes/mode/lua";
+import { to
