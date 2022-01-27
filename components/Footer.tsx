384137636336a3233f68acb3c21405486c231196
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-01-27 15:34:14 +0000

    workqueue: skip nr_running sanity check in worker_enter_idle() if trustee is active

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="text-center h-16 sm:h-20 w-full sm:
+    <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
+      <div>
+    
