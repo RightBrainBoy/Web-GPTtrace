6d624fe90b4c2d6f8befb71e22872fa8a6c218f9
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-02-22 21:21:34 +0000

    added unit testing

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,4 +4,5 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex justif
+    <div className="flex justify-between items-center w-full mt-5 border-b-2 pb-1 sm:px-4 px-2 ">
+      <Link href="/" className="flex space-x-3 ml-10
