9bf5dc48ae6b1e0abb73d3b42f87060535c2936f
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-02-23 09:27:41 +0000

    matroxfb: restore the registers M_ACCESS and M_PITCH

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,4 +5,8 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="flex justify-between items-center w-full mt-5 border-b-2 pb-1 sm:px-4 px-2 ">
-      <Link href="/" className="flex space-x-3 ml-10
+      <Link href="/" className="flex space-x-3 ml-10">
+        <Image
+          alt="header text"
+          src="/OpenCopilot.png"
+          className="sm:w-12 sm:h-12 
