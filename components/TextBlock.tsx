40acd031fa4a2aa3295164e3f5b2e0b5a76d57d6
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-04-15 10:59:37 +0000

    v5.3.0

--- components/TextBlock.tsx
+++ components/TextBlock.tsx
@@ -13,4 +13,9 @@ export const TextBlock: React.FC<Props> = ({
     <textarea
       className="min-h-[500px] w-full bg-[#1A1B26] p-4 text-[15px] text-neutral-200 focus:outline-none"
       style={{ resize: 'none' }}
-  
+      value={text}
+      onChange={(e) => onChange(e.target.value)}
+      disabled={!editable}
+    />
+  );
+};
