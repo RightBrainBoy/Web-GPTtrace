b501e985d888213ffa5603ab93b6f952a727a83a
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-08-06 19:21:37 +0000

    updated _config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Inter } from "next
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+function App({ Component, pageProps }: AppProps<{}>) {
+  return (
+    
