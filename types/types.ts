1efd11760ce0000b357a1d64e8af17cd7744cf1d
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-08-23 10:43:59 +0000

    impeachment

--- types/types.ts
+++ types/types.ts
@@ -1,2 +1,11 @@
 export type OpenAIModel = 'gpt-3.5-turbo' | 'gpt-4';
-export type BPF = 'libbpf' | 'bpftrace
+export type BPF = 'libbpf' | 'bpftrace';
+
+export interface TranslateBody {
+  help_doc: string;
+  language: string;
+  model: OpenAIModel;
+  apiKey: string;
+}
+
+export interfac
