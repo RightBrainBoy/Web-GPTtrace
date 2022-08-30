4d3b2fed2e65b34a887e68fc9b3b1cf55153bdef
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-08-30 21:17:27 +0000

    HID: add ASUS AIO keyboard model AK1D

--- utils/eBPFDatabase.tsx
+++ utils/eBPFDatabase.tsx
@@ -2,3 +2,5 @@ export const getExamples = async (query: string, apiKey: string) => {
   const { MemoryVectorStore } = await import("langchain/vectorstores/memory");
   const { OpenAIEmbeddings } = await import("langchain/embeddings/openai");
   
+  const vectorStore = await MemoryVectorStore.fromTexts(
+    ["example: Write a BPF code that traces
