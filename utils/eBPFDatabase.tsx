9c6d7d0d8ad737e1e1746b235241ea66e0e931a3
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-08-30 18:17:14 +0000

    image test

--- utils/eBPFDatabase.tsx
+++ utils/eBPFDatabase.tsx
@@ -1,2 +1,4 @@
 export const getExamples = async (query: string, apiKey: string) => {
-  const { MemoryVecto
+  const { MemoryVectorStore } = await import("langchain/vectorstores/memory");
+  const { OpenAIEmbeddings } = await import("langchain/embeddings/openai");
+  
