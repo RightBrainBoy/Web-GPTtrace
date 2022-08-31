fcfd17bfadce517b85e71a5268fdcbdfd8077dd8
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-08-31 21:32:06 +0000

    added client

--- utils/eBPFDatabase.tsx
+++ utils/eBPFDatabase.tsx
@@ -3,4 +3,4 @@ export const getExamples = async (query: string, apiKey: string) => {
   const { OpenAIEmbeddings } = await import("langchain/embeddings/openai");
   
   const vectorStore = await MemoryVectorStore.fromTexts(
-    ["example: Write a BPF code that traces block I/O operations and displays the latency for each operation, along with the disk, process, and PID associated with it.\n\n```\n#ifndef BPFTRACE_HAVE_BTF\n#include <linux/blkdev.h>\n#include <linux/blk-mq.h>\n#endif\n\nBEGIN\n{\n\tprintf(\"%-12s %-7s %-16s %-6s %7s\\n\", \"TIME(ms)\", \"DISK\", \"COMM\", \"PID\", \"LAT(ms)\");\n}\n\nkprobe:blk_account_io_
+    ["example: Write a BPF code that traces block I/O operations and displays the latency for each operation, along with the disk, process, and PID associated with it.\n\n```\n#ifndef BPFTRACE_HAVE_BTF\n#include <linux/blkdev.h>\n#include <linux/blk-mq.h>\n#endif\n\nBEGIN\n{\n\tprintf(\"%-12s %-7s %-16s %-6s %7s\\n\", \"TIME(ms)\", \"DISK\", \"COMM\", \"PID\", \"LAT(ms)\");\n}\n\nkprobe:blk_account_io_start,\nkprobe:__blk_account_io_start\n{\n\t@start[arg0] = nsecs;\n\t@iopid[arg0] = pid;\n\t@iocomm[arg0] = comm;\n\
