8ec5c381160fab4dfe85e1b4eb35c7ead00603af
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2022-08-31 10:54:26 +0000

    Bold

--- utils/eBPFDatabase.tsx
+++ utils/eBPFDatabase.tsx
@@ -3,4 +3,4 @@ export const getExamples = async (query: string, apiKey: string) => {
   const { OpenAIEmbeddings } = await import("langchain/embeddings/openai");
   
   const vectorStore = await MemoryVectorStore.fromTexts(
-    ["example: Write a BPF code that traces block I/O operations and displays the latency for each operation, along with the disk, process, and PID associated with
+    ["example: Write a BPF code that traces block I/O operations and displays the latency for each operation, along with the disk, process, and PID associated with it.\n\n```\n#ifndef BPFTRACE_HAVE_BTF\n#include <linux/blkdev.h>\n#include <linux/blk-mq.h>\n#endif\n\nBEGIN
