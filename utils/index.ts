1d825115c792fa95b313129a712877e767f00d4c
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2023-10-03 21:50:53 +0000

    Update main.asm

--- utils/index.ts
+++ utils/index.ts
@@ -7,4 +7,6 @@ const createPrompt = async (
 ) => {
   const complexExamples = await getExamples(query, apiKey);
   const PROMPT = `
-  Please create a BPFTrace program that accomp
+  Please create a BPFTrace program that accomplishes the following task: ${query}
+  The program should be syntactically correct and ready for execution. 
+  You can as
