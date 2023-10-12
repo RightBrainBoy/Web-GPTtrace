a264a6e559a8d95d99c98ca2f6ecce9726e5b3ae
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2023-10-12 10:38:45 +0000

    can: peak_pci: prevent use after free at netdev removal

--- utils/index.ts
+++ utils/index.ts
@@ -9,4 +9,5 @@ const createPrompt = async (
   const PROMPT = `
   Please create a BPFTrace program that accomplishes the following task: ${query}
   The program should be syntactically correct and ready for execution. 
-  You can as
+  You can assume that the required BPFTrace probes and functions are available.
+  Below are some simple examples of bpftrace usa
