6991e1cceb475d14ed25ebb7dea0cdbdb3528a04
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2023-10-13 14:53:11 +0000

    adds post

--- utils/index.ts
+++ utils/index.ts
@@ -10,4 +10,7 @@ const createPrompt = async (
   Please create a BPFTrace program that accomplishes the following task: ${query}
   The program should be syntactically correct and ready for execution. 
   You can assume that the required BPFTrace probes and functions are available.
-  Below are some simple examples of bpftrace usa
+  Below are some simple examples of bpftrace usage:
+  
+  # trace processes calling sleep
+  'kprobe:do_nanosleep { printf("PID %d sleeping...\n", p
