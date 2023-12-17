81cbd3030044c6f082f9c496d8013f58571f23bc
RightBrainBoy <158732435+RightBrainBoy@users.noreply.github.com>
2023-12-17 05:44:21 +0000

    xml fix

--- utils/index.ts
+++ utils/index.ts
@@ -72,3 +72,7 @@ export const OpenAIStream = async (
 
   const res = await fetch(`https://api.openai.com/v1/chat/completions`, {
     headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key || process.env.OPENAI_API_KEY}`,
+    },
+    metho
